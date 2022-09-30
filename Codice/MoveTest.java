00250dafa106cef4703eb28dc82d8127a19cf721
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2022-09-30 22:50:38 +0200

    MoveTest_cerealBox

--- Codice/MoveTest.java
+++ Codice/MoveTest.java
@@ -1,7 +1,6 @@
 import java.awt.Rectangle;
 
 public class MoveTest{
-
 	public static void main(String args[]){
 		Rectangle cerealBox = new Rectangle(5,10,20,30);
 		System.out.println(cerealBox);
