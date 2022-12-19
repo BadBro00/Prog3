64e29874a2c1d48d3b0f4a043e41a7a119c331bb
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2022-12-19 16:40:35 +0100

    Costrutti

---
+++ Lezioni/Lezione4.java
@@ -0,0 +1,51 @@
+/*
+                                                    |DECISIONI ED ITERAZIONI|
+-----------------------------------------------------------------------------------------------------------------------------
+In ogni linguaggio di programmazione esistono costrutti che permettono di iterare o di effettuare decisioni. In Java esistono i costrutti:
+
+-if/elseif/else
+-for
+-while (do-while,while...)
+-switch
+-foreach
+- ?, : 
+*/
+
+//if/elseif/else
+if(condition){
+  //Codice
+}else if(condition2){
+  //Codice
+}else{
+  //Codice
+}
+
+//for
+for(/*iterazioni*/){
+  //Codice
+}
+
+//while
+while(/*condizione*/){
+  //Codice
+}
+
+//switch
+switch(variabile){
+    case ...:
+      //Codice
+    break;
+    ...
+    default:
+      //Codice
+}
+
+//foreach
+for(var_temp : oggetto_iterabile){
+  //Codice
+}
+
+// ? :
+var1 > var2 ? var2++ : var1++;
+
+  
