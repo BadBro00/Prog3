41537c8a73b8c5afcd629af5ad97fa1232b86a36
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2022-12-19 15:03:06 +0100

    Lezione2

---
+++ Lezioni/Lezione2.java
@@ -0,0 +1,20 @@
+/*
+                                            |Approcci alla programmazione OO|
+---------------------------------------------------------------------------------------------------------------------
+La classe contenuta nel file dev'essere public
+
+Il metodo main, non agendo su un oggetto, e' istanziato come static
+
+Gli argomenti passati da riga di comando al main sono dichiarati come String args[]
+
+Per stampare a video, si usa il comando System.out.println(...):
+      i) System -> Classe System
+      ii) out -> Oggetto della classe System
+      iii) println() -> metodo dell'oggetto out
+*/
+
+public class Lezione2{
+  public static void main(String args[]){
+    System.out.println("Hello World!");
+  }
+}
