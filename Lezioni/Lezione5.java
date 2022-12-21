9b0103a45b951f9dcabea76af63fa382b1902786
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2022-12-21 17:43:18 +0100

    Create Lezione5.java

---
+++ Lezioni/Lezione5.java
@@ -0,0 +1,17 @@
+/*
+                                                |PARADIGMI DELLA PROGRAMMAZIONE OO|
+------------------------------------------------------------------------------------------------------------------------------
+I paradigmi principali della programmazione OO sono:
+
+  1)Incapsulamento
+  2)Ereditarieta
+  3)Polimorfismo
+  4)Astrazione
+  5)Riuso
+  
+            Astrazione:
+Ci sono 3 tipi di astrazione:
+    i) Astrazione Funzionale : Implementazione di un metodo
+    ii) Astrazione dei Dati : definiamo una classe, raccogliendo in essa solo le caratteristiche e le funzionalità essenziali degli oggetti che essa deve definire
+    iii) Astrazione del Sistema : definiamo un’applicazione nei termini delle classi essenziali che devono soddisfare agli scopi dell’applicazione stessa 
+
